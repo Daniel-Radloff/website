7f7db2e981b3c86443091739d95b8422655c8121
GD Radloff <danielrad082@gmail.com>
2024-02-23 17:01:14 +0200

    Refactored and Documented notion.ts

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -1,7 +1,13 @@
 import { Client } from '@notionhq/client';
+import { GetPageResponse } from '@notionhq/client/build/src/api-endpoints';
 import { NotionToMarkdown } from 'notion-to-md';
 
-export const notion = new Client({
+// Exports Some functions to get Documents that we can display posts from notion.
+// Contains all the notion logic
+
+
+//Helpers
+const notion = new Client({
     auth: process.env.NOTION_TOKEN,
 })
 
@@ -15,14 +21,17 @@ const getTags = (tags: any) => {
 
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
-export const getPageMetaData = (post: any) => {
+const getPageMetaData = (post: any) => {
+    const formatDate = (datestring: string) => {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let date = new Date(datestring);
 
-    const getTags = (tags: any) => {
-        const allTags = tags.map((tag: any, index: number) => {
-            return { name: tag.name, color: tag.color };
-        });
+        const day = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+        let today = `${month} ${day}, ${year}`;
 
-        return allTags;
+        return today;
     };
 
     return {
@@ -30,32 +39,14 @@ export const getPageMetaData = (post: any) => {
         title: post.properties.Name.title[0].plain_text,
         tags: getTags(post.properties.Tags.multi_select),
         description: post.properties.Description.rich_text[0].plain_text,
-        date: getToday(post.properties.Date.date.start),
-        last_edited: getToday(post.properties.Date.last_edited_time),
-        slug: post.properties.Slug.rich_text[0].plain_text,
-        // image: post.properties.Image.files[0].file.url,
+        date: formatDate(post.properties.Date.date.start),
+        last_edited: formatDate(post.properties.Date.last_edited_time),
         image: "",
         author: post.properties.Author.rich_text[0].plain_text,
     };
 };
 
-export const getToday = (datestring: string) => {
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    let date = new Date();
-
-    if (datestring) {
-        date = new Date(datestring);
-    }
-
-    const day = date.getDate();
-    const month = months[date.getMonth()];
-    const year = date.getFullYear();
-    let today = `${month} ${day}, ${year}`;
-
-    return today;
-};
-
+// This section has everything to do with getting information about projects
 export const getProjects = async () => {
     const projects = await notion.databases.query({
         database_id: process.env.PROJECTS_ID as string,
@@ -73,46 +64,34 @@ export const getProjects = async () => {
             description: project.properties.Description.rich_text[0].plain_text,
             link: project.properties.URL.url,
             stack: project.properties.Stack.multi_select.map((item: any) => item.name),
-            id: project.properties.ID.rich_text[0].plain_text
+            id: project.id
         }
     });
 }
 
 export const getProject = async (id: string) => {
-    const projects = await notion.databases.query({
-        database_id: process.env.PROJECTS_ID as string,
-        filter: {
-            property: "ID",
-            formula: {
-                string: {
-                    equals: id,
-                },
-            },
-        }
+    const project = await notion.pages.retrieve({
+        page_id : id
     });
-
-    const project = projects.results[0] as any;
-
     const mdblocks = await n2m.pageToMarkdown(project.id);
     const mdString = n2m.toMarkdownString(mdblocks);
 
 
 
-    const item = {
+    return {
         name: project.properties.Name.title[0].plain_text,
         description: project.properties.Description.rich_text[0].plain_text,
         link: project.properties.URL.url,
         stack: getTags(project.properties.Stack.multi_select),
-        id: project.properties.ID.rich_text[0].plain_text,
+        id: project.id,
         content: mdString.parent ? mdString.parent : '',
     }
-
-    return item;
 }
 
+// This section has everything to do with getting blog posts.
 export const getPosts = async () => {
     const posts = await notion.databases.query({
-        database_id: process.env.DATABASE_ID as string,
+        database_id: process.env.BLOG_DB_ID as string,
         filter: {
             property: "Published",
             checkbox: {
@@ -134,7 +113,7 @@ export const getPosts = async () => {
 
 export const getPostsByTag = async (tag: string) => {
     const posts = await notion.databases.query({
-        database_id: process.env.DATABASE_ID as string,
+        database_id: process.env.BLOG_DB_ID as string,
         filter: {
             property: "Tags",
             multi_select: {
@@ -157,7 +136,7 @@ export const getPostsByTag = async (tag: string) => {
 export const getPostsByAuthor = async (author: string) => {
 
     const response = await notion.databases.query({
-        database_id: process.env.DATABASE_ID as string,
+        database_id: process.env.BLOG_DB_ID as string,
         filter: {
             property: "Author",
             formula: {
@@ -180,21 +159,12 @@ export const getPostsByAuthor = async (author: string) => {
 
 }
 
-export const getPost = async (slug: string) => {
+export const getPost = async (id: string) => {
 
-    const response = await notion.databases.query({
-        database_id: process.env.DATABASE_ID as string,
-        filter: {
-            property: "Slug",
-            formula: {
-                string: {
-                    equals: slug,
-                },
-            },
-        },
+    const page = await notion.pages.retrieve({
+        page_id : id
     });
 
-    const page = response.results[0];
     const metadata = getPageMetaData(page);
     const mdblocks = await n2m.pageToMarkdown(page.id);
     const mdString = n2m.toMarkdownString(mdblocks);
